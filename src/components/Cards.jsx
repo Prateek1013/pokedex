6b9bef4cb7337846134642120167698978a76f9d
Prateek1013 <prateek9771196112@gmail.com>
2023-12-25 03:02:50 +0530

    tried fetch img

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,24 +1,24 @@
 import { Card } from "antd";
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 
 const img_url = 'https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/';
 
 const Cards = (props) => {
-    const [types,settypes]=useState([]);
-    const { name, url ,setmodalopen,setdetails} = props;
+    const [types, settypes] = useState([]);
+    const { name, url, setmodalopen, setdetails } = props;
     var uri = url.split('/');
     const id = uri[uri.length - 2];
     useEffect(() => {
-        const func=()=>{
-            fetch(url).then(res=>res.json())
-    .then(jsondata=>{
-         settypes(jsondata.types);
-        
-    }).catch(err=>console.log(err.message))
+        const func = () => {
+            fetch(url).then(res => res.json())
+                .then(jsondata => {
+                    settypes(jsondata.types);
+        }).catch(err=>console.log(err.message))
         }
-        func()
+        func();
+        
     }, [props]);
-    
+
     return (
         <div >
             <Card
@@ -27,15 +27,15 @@ const Cards = (props) => {
                     width: 240,
                 }}
                 cover={<img alt="example" src={`${img_url}${id}.svg`} />}
-                onClick={()=>{
-                    setdetails({name,url});
+                onClick={() => {
+                    setdetails({ name, url });
                     setmodalopen(true);
                 }}
             >
-                 <h3>{name}</h3>
-                {types.map(item=>(
+                <h3>{name}</h3>
+                {types.map(item => (
                     <p>{item.type.name}</p>
-                   
+
                 ))}
 
             </Card>
